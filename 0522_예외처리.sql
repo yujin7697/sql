ead3ba5c09a2ac1f91319ef539f7f951cf7184d9
jangyujin <juj7697@gmail.com>
2023-05-22 14:32:07 +0900

    0522

--- 0522_예외처리.sql
+++ 0522_예외처리.sql
@@ -55,17 +55,36 @@ call Exception_Test04();
 
 -- 05 error_log 기록하는 테이블처리
 create table tbl_std(id varchar(20) primary key, name char(10), age int);
-create table tbl_std_errlog(error_date date, error_code int, error_msg text);
+drop table tbl_std_errlog;
+create table tbl_std_errlog(error_date datetime, error_code int, error_msg text);
 show errors;
 
 delimiter $$
 create procedure tbl_std_proc(in id varchar(20), in name char(10),in age int)
 begin
+	declare error_code varchar(5);
+    declare error_message varchar(255);
 	-- pk 중복 예외 처리
 	declare continue handler for 1062
     begin
 		show errors;
-        get diagnostics condition 1;
+        get diagnostics condition 1
+			error_code=mysql_errno,
+            error_message=message_text;
+		-- select error_code,error_message;
+        insert into tbl_std_errlog values(now(),error_code,error_message);
+    end;
+    -- exception code 1265
+    declare continue handler for 1265
+    begin
+		show errors;
+        get diagnostics condition 1
+			error_code=mysql_errno,
+            error_message=message_text;
+		-- select error_code,error_message;
+        insert into tbl_std_errlog values(now(),error_code,error_message);
+        set age=0;
+        insert into tbl_std values(id,name,age);
     end;
 	insert into tbl_std values(id,name,age);
     select * from tbl_std;
@@ -73,5 +92,7 @@ end $$
 delimiter ;
 call tbl_std_proc('aa','홍길동',10);
 call tbl_std_proc('ab','남길동',20);
+call tbl_std_proc('ac','남길동','5-');
+select * from tbl_std_errlog;
 select * from tbl_std;
 show errors;
