0398cab6fcfa7d1501ef3fb172c0a106a54ed6e6
jangyujin <juj7697@gmail.com>
2023-05-22 15:07:51 +0900

    0522

--- 0522_예외처리.sql
+++ 0522_예외처리.sql
@@ -52,15 +52,16 @@ end $$
 delimiter ;	
 
 call Exception_Test04();
-
+-- 시험 문제로 나올ㄹ거임
 -- 05 error_log 기록하는 테이블처리
 create table tbl_std(id varchar(20) primary key, name char(10), age int);
 drop table tbl_std_errlog;
 create table tbl_std_errlog(error_date datetime, error_code int, error_msg text);
 show errors;
 
+drop procedure tbl_std_proc;
 delimiter $$
-create procedure tbl_std_proc(in id varchar(20), in name char(10),in age int)
+create procedure tbl_std_proc(in id varchar(20), in name char(10),in age varchar(10))
 begin
 	declare error_code varchar(5);
     declare error_message varchar(255);
@@ -92,7 +93,32 @@ end $$
 delimiter ;
 call tbl_std_proc('aa','홍길동',10);
 call tbl_std_proc('ab','남길동',20);
-call tbl_std_proc('ac','남길동','5-');
+call tbl_std_proc('af','남길동','5-');
 select * from tbl_std_errlog;
 select * from tbl_std;
 show errors;
+
+-- 프로시저(예외처리 + 트랜젝션)
+delete from tbl_std;
+select * from tbl_std;
+drop procedure tbl_std_proc_tx;
+delimiter $$
+create procedure tbl_std_proc_tx()
+begin
+	declare exit handler for sqlexception
+    begin
+		show errors;
+		rollback; -- 문제가 발생했을때 rollback처리
+    end;
+    start transaction;
+		insert into tbl_std values('f','hoho',11);
+		insert into tbl_std values('g','hoho',12);
+		insert into tbl_std values('f','hoho',13);
+		insert into tbl_std values('i','hoho',14);
+        commit; -- 문제가 발생하지 않았을때 정상 처리
+		select * from tbl_std;
+end $$
+delimiter ;
+call tbl_std_proc_tx();
+select * from tbl_std;
+
